9f56864a49a193817f98f1254f516783e738a115
Bek Abdik <beknazar23@gmail.com>
2023-03-25 17:04:30 +0600

    Update title to 'Beknazar' on index page.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ARZUBEK' });
+  res.render('index', { title: 'Beknazar' });
 });
 
 module.exports = router;
